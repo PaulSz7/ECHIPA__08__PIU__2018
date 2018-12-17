f15f16ee9850f4034914cddd4845a63dfeffb665
attila-wolfpack <attila@wolfpack-digital.com>
2018-12-17 22:05:19 +0200

    Log in

--- app/src/main/java/com/example/piu/studentapp/LoginActivity.java
+++ app/src/main/java/com/example/piu/studentapp/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.example.piu.studentapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static java.lang.Thread.sleep;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,7 +15,71 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
 
+    protected void loginClick(View view) {
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
 
+        TextView usernameError = (TextView) findViewById(R.id.usernameError);
+        TextView passwordError = (TextView) findViewById(R.id.passwordError);
+
+        boolean letLogin = true;
+
+        if (username.getText().length() < 1) {
+            usernameError.setText("Username cannot be empty!");
+            letLogin = false;
+        }
+        else if (username.getText().length() < 3) {
+            usernameError.setText("Username is too short!");
+            letLogin = false;
+        }
+        else {
+            usernameError.setText("");
+        }
+
+        if (password.getText().length() < 1) {
+            passwordError.setText("Password cannot be empty!");
+            letLogin = false;
+        }
+        else if (password.getText().length() < 3) {
+            passwordError.setText("Password is too short!");
+            letLogin = false;
+        }
+        else {
+            passwordError.setText("");
+        }
+
+        TextView resultMsg = (TextView) findViewById(R.id.resultMsg);
+        if (!letLogin) return;
+
+        if (username.getText().toString().equals("profesor") && password.getText().toString().equals("password")) {
+            resultMsg.setText("Login successfull (profesor)!");
+            resultMsg.setTextColor(Color.GREEN);
+
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        //            Intent intent = new Intent(this, OffersListActivity.class);
+        //            startActivity(intent);
+        }
+        else if (username.getText().toString().equals("student") && password.getText().toString().equals("password")) {
+            resultMsg.setText("Login successfull (student)!");
+            resultMsg.setTextColor(Color.GREEN);
+
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //            Intent intent = new Intent(this, OffersListActivity.class);
+            //            startActivity(intent);
+        }
+        else {
+            resultMsg.setText("Login failed\nUsername or password is incorrect!");
+            resultMsg.setTextColor(Color.RED);
+        }
     }
 }
